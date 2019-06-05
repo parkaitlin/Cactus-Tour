f331fc5f5f88265e6a5a74f04347085ac2caab84
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-06-04 19:35:56 -0700

    reset app.js css | set up router | create navbar | create basic register & login component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import styled from 'styled-components';
+
+import * as routes from './constants/routes';
+import {Switch, Route} from 'react-router-dom';
+
+import Navbar from './Navbar/Navbar'
+
+
+
+
+class App extends Component {
+  state = {
+    logged: false
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path={routes.HOME} render={()=> <div>HOME PAGE</div>} />
+          <Route exact path={routes.LOGIN} render={()=> <div>LOGIN PAGE</div>} />
+          <Route exact path={routes.MEMBER} render={()=> <div>MEMBERSHIP REGISTRATION PAGE</div>} />
+          <Route exact path={routes.SCHEDULE} render={()=> <div>SCHEDULE PAGE</div>} />
+          <Route exact path={routes.MLIST} render={()=> <div>MONEY LIST PAGE</div>} />
+          <Route exact path={routes.PAST} render={()=> <div>PAST RESULTS PAGE</div>} />
+          <Route exact path={routes.POLICY} render={()=> <div>POLICIES PAGE</div>} />
+          <Route exact path={routes.CONTACT} render={()=> <div>CONTACT US PAGE</div>} />
+          <Route exact path={routes.ABOUT} render={()=> <div>ABOUT PAGE</div>} />          
+        </Switch>
+
+      </div>
+    )
+  }
 }
 
 export default App;
